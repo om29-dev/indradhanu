15e1f2be034717ffeaf5f3f88b522f8d29c056e7
om29-dev <ombharambe901@gmail.com>
2025-08-30 15:02:40 +0530

    Remove backend setup steps from the setup script

--- setup.bat
+++ setup.bat
@@ -1,11 +1,3 @@
-@echo off
-REM Setup script for indradhanu project
-REM Create and activate Python virtual environment, then install backend dependencies
-cd backend
-python -m venv .venv
-call .venv\Scripts\activate
-pip install --upgrade pip
-pip install -r requirements.txt
 
 cd ..
 REM Install frontend Node.js dependencies
